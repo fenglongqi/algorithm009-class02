a269698511b316de5c99316defc0e8f4db380910
全海龙 <wb.quanhailong@mesg.corp.netease.com>
2020-07-12 20:40:09 +0800

    week8

---
+++ Week_08/231.power-of-two.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+
+    if(n<1){
+        return false;
+    }
+    return !(n & (n-1));
+
+};
---
+++ Week_08/56.merge-intervals.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+    if (intervals.length === 0) return []
+
+    intervals.sort((a, b) => a[0] - b[0]);
+    let res = [];
+    let idx = -1;
+    for (let interval of intervals) {
+        if (idx == -1 || interval[0] > res[idx][1]) {
+            res.push(interval);
+            idx++;
+        } else {
+            res[idx][1] = Math.max(res[idx][1], interval[1]);
+        }
+    }
+    return res;
+};
